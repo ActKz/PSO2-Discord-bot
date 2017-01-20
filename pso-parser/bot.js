6883b8361cce582a18243bf57ada736ad0ec9ac5
actkz <actkz.cs02@g2.nctu.edu.tw>
2017-01-21 04:04:18 +0800

    Simplify the code

--- pso-parser/bot.js
+++ pso-parser/bot.js
@@ -1,8 +1,7 @@
 const config = require('../config.json');
-var request = require('request')
-  , cheerio = require('cheerio')
-  , jquery = require('jquery');
-var Discord = require("discord.js");
+var request = require('request'),
+    cheerio = require('cheerio'),
+    Discord = require("discord.js");
 var bot = new Discord.Client();
 
 const helplist = `
@@ -15,103 +14,80 @@ const helplist = `
     Notice:'＊' means special events or server maintainence.
 `
 
-bot.on("message", msg => {
-  if (msg.content == "!help") {
-      msg.reply(helplist);
-  }
-  if (msg.content.startsWith("!twit")){
-      var url = "https://twitter.com/pso2_emg_hour?lang=zh-tw";
-      var res = "\nLatest\n-------------------\n";
-      var tweets = 3;
-      let argv = msg.content.split(" ");
-      if(argv.length > 1 && parseInt(argv[1])!= NaN && argv[1] > 0){
-          tweets = argv[1];
-      }
-      request(url, function(err, resp, body){
-          $ = cheerio.load(body);
-          $('.TweetTextSize').each(function(i, s){
-              if(i > 0 && i <= tweets){
-                console.log($(s).text());
-                res += $(s).text()+"\n";
-                res += "-------------------\n";
-              }
-          });
-          msg.reply(res);
-      });
-  }
-  if (msg.content == "!emerg") {
-      var url = "http://pso2.swiki.jp/";
-      var res = "\n";
-      request(url, function(err, resp, body){
-        $ = cheerio.load(body);
-        $('.style_table').eq(4).children('thead').children('tr').children('td').each(function(i, s){
-            if($(s).text() == ""){
-                res += "　　　|";
-            }else if($(s).text() != "終日ブーストイベント00:00～23:59"){
-                res += $(s).text().slice(0,-1).replace(/[0-9]/g, function(m){
-                    return {
-                        '0': '０',
-                        '1': '１',
-                        '2': '２',
-                        '3': '３',
-                        '4': '４',
-                        '5': '５',
-                        '6': '６',
-                        '7': '７',
-                        '8': '８',
-                        '9': '９',
-                    }[m];
-                });
-                if($(s).text().length < 3){
-                    res += '　|';
-                }else{
-                    res += '|';
-                }
-            }else {
-                res = res.slice(0,-1);
-            }
-        });
-        res += "\n";
-        $('.style_table').eq(4).children('tbody').children('tr').each(function(i, p){
-            $(p).children('td').each(function(j, s){
-              if($(s).text() == ""){
-                  res += "　　|";
-              } else if(j != 0){
-                  var colspan = $(s).attr('colspan');
-                  if(typeof colspan !== typeof undefined && colspan !== false){
-                      res += "＊＊|".repeat(colspan);
-                  }else if($(s).text().length > 2){
-                      res += "＊＊|";
-                  }else{
-                      res += $(s).text()+"|";
-                  }
-              } else{
-                res += $(s).text().replace(/[0-9()]/g, function(m){
-                    return {
-                        '0': '０',
-                        '1': '１',
-                        '2': '２',
-                        '3': '３',
-                        '4': '４',
-                        '5': '５',
-                        '6': '６',
-                        '7': '７',
-                        '8': '８',
-                        '9': '９',
-                        '(': '',
-                        ')': ''
-                    }[m];
-                });
-                  res += "|";
-              }
-            });
-            res = res.slice(0,-4);
-            res += "\n";
-        });
-        console.log(res);
-        msg.reply(res);
+// Also delete '(' ')'
+function convertFullWidth(str){
+    return str.replace(/[0-9()]/g, function(m){
+        return {
+            '0': '０',
+            '1': '１',
+            '2': '２',
+            '3': '３',
+            '4': '４',
+            '5': '５',
+            '6': '６',
+            '7': '７',
+            '8': '８',
+            '9': '９',
+            '(': '',
+            ')': ''
+        }[m];
     });
-  }
+}
+
+bot.on("message", msg => {
+    if (msg.content == "!help") {
+        msg.reply(helplist);
+    }
+    if (msg.content.startsWith("!twit")){
+        let url = "https://twitter.com/pso2_emg_hour?lang=zh-tw",
+            div = "-------------------\n",
+            res = "\nLatest\n" + div,
+            tweets = 3,
+            argv = msg.content.split(" ");
+        if(argv.length > 1 && parseInt(argv[1])!= NaN && argv[1] > 0){
+            tweets = argv[1];
+        }
+        request(url, function(err, resp, body){
+            $ = cheerio.load(body);
+            $('.TweetTextSize').each(function(i, s){
+                if(i > 0 && i <= tweets){
+                  console.log($(s).text());
+                  res += $(s).text()+"\n"+div;
+                }
+            });
+            msg.reply(res);
+        });
+    }
+    if (msg.content == "!emerg") {
+        let url = "http://pso2.swiki.jp/",
+            res = "\n";
+        request(url, function(err, resp, body){
+            $ = cheerio.load(body);
+            $('.style_table').eq(4).children('thead').children('tr').children('td').each(function(i, s){
+                let len = $(s).text().length;
+                if($(s).text().length <= 3){
+                    res += '　'.repeat(3-len) + convertFullWidth($(s).text().slice(0,-1)) + "|";
+                }
+            });
+            res = res.slice(0,-1)+"\n";
+            $('.style_table').eq(4).children('tbody').children('tr').each(function(i, p){
+                $(p).children('td').each(function(j, s){
+                    if($(s).text() == ""){
+                        res += "　　|";
+                    } else if(j != 0){
+                        let text = ($(s).text().length<=2) ? $(s).text()+"|" : "＊＊|",
+                            colspan = $(s).attr('colspan') || 1;
+                        res += text.repeat(colspan);
+                    } else{
+                        res += convertFullWidth($(s).text()) + "|";
+                    }
+                });
+                res = res.slice(0,-4) + "\n"; // forget why 4!
+            });
+            console.log(res);
+            msg.reply(res);
+      });
+    }
 });
 
 bot.login(config.token);
