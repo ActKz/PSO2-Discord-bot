3b1b77636290941befa4cb53e3aea210fd842993
tryu4wk4 <tryu4wk4@gmail.com>
2019-06-09 20:57:18 +0800

    Fix bugs, refactor functions

--- pso-parser/bot.js
+++ pso-parser/bot.js
@@ -25,6 +25,8 @@ function buildTooLong(txt){
     }else{
         if(txt in TooLong){
             return TooLong[txt].repeat(2)+'|';
+		}else if(txt.match(/[0-9][0-9]:[0-9][0-9]/)){
+            return "　　|";
         }else{
             let spec = charlist.pop();
             TooLong[txt] = spec;
@@ -36,7 +38,7 @@ function buildTooLong(txt){
 
 // Also delete '(' ')'
 function convertFullWidth(str){
-    return str.replace(/[0-9()]/g, function(m){
+    return str.replace(/[0-9() ]/g, function(m){
         return {
             '0': '０',
             '1': '１',
@@ -48,6 +50,7 @@ function convertFullWidth(str){
             '7': '７',
             '8': '８',
             '9': '９',
+			' ': '　',
             '(': '',
             ')': ''
         }[m];
@@ -60,73 +63,89 @@ function getCurrJPDate(){
     return JPDate.getDate();
 }
 
+function parseHtml(msg, body){
+	headlen = 0,
+    res = "\n";
+    $ = cheerio.load(body);
+    $('.style_table').eq(1).children('thead').children('tr').children('td').each(function(i, s){
+        let len = $(s).text().length;
+        if($(s).text().length <= 3){
+            res += '　'.repeat(3-len) + convertFullWidth($(s).text().slice(0,-1)) + "|";
+    			headlen += 1;
+        }
+    });
+    res = res.slice(0,-1)+"\n";
+    let rowspan = 0, rowshift = [0];
+    $('.style_table').eq(1).children('tbody').children('tr').each(function(i, p){
+        if(rowshift[rowshift.length-1] != 0){
+            res += "　　　|";
+            $(p).children('td').each(function(j, s){
+                console.log(rowshift);
+                res += "　　|".repeat(rowshift.shift());
+                let colspan = $(s).attr('colspan') || 1;
+                res += buildTooLong($(s).text()).repeat(colspan);
+                if(colspan > 1){
+                    res += "　　|".repeat(rowshift.shift());
+                }
+            });
+            res += "　　|".repeat(rowshift.shift());
+            rowshift = [0];
+        }else{
+        $(p).children('td').each(function(j, s){
+            if(j != 0){
+                let text = ($(s).text() == "")?"　　|":
+                    buildTooLong($(s).text()),
+                    colspan = $(s).attr('colspan') || 1;
+                if($(s).attr('rowspan') == rowspan){
+                    rowshift[rowshift.length-1] += parseInt(colspan);
+                } else {
+                    rowshift.push(0);
+                }
+                res += text.repeat(colspan);
+            } else{
+                rowspan = $(s).attr('rowspan');
+    				console.log($(s).text().length);
+    				if($(s).text().length == 4)
+    					res +=  "　";
+                res += convertFullWidth($(s).text()) + "|";
+            }
+    			if(rowshift[0] == headlen){
+    				rowshift[0] = 0;
+    			}
+        });
+        }
+
+        res = res.slice(0,-4) + "\n"; // forget why 4!
+    });
+    for (var key in TooLong){
+        res += TooLong[key]+": "+key+"\n";
+    }
+    if( eventList.e == undefined || eventList.e != res){
+        eventList.e = res;
+        msg.reply(res);
+    }else{
+        msg.reply(eventList.e);
+    }
+
+}
+
 function updateEvent(msg){
-    let url = "http://pso2.swiki.jp/index.php?%E7%B7%8A%E6%80%A5%E6%8E%B2%E7%A4%BA%E6%9D%BF%2F%E4%BA%88%E5%AE%9A%E8%A1%A8",
+    let url = "https://pso2m.swiki.jp/index.php?%E7%B7%8A%E6%80%A5%E6%8E%B2%E7%A4%BA%E6%9D%BF%2F%E4%BA%88%E5%AE%9A%E8%A1%A8",
 		headlen = 0,
         res = "\n";
-    request(url, function(err, resp, body){
+    request({uri: url, strictSSL: false}, function(err, resp, body){
         if(err){
-            msg.reply("ERROR: "+err);
-            return;
-        }
-        $ = cheerio.load(body);
-        $('.style_table').eq(1).children('thead').children('tr').children('td').each(function(i, s){
-            let len = $(s).text().length;
-            if($(s).text().length <= 3){
-                res += '　'.repeat(3-len) + convertFullWidth($(s).text().slice(0,-1)) + "|";
-				headlen += 1;
-            }
-        });
-        res = res.slice(0,-1)+"\n";
-        let rowspan = 0, rowshift = [0];
-        $('.style_table').eq(1).children('tbody').children('tr').each(function(i, p){
-            if(rowshift[rowshift.length-1] != 0){
-                res += "　　　|";
-                $(p).children('td').each(function(j, s){
-                    console.log(rowshift);
-                    res += "　　|".repeat(rowshift.shift());
-                    let colspan = $(s).attr('colspan') || 1;
-                    res += buildTooLong($(s).text()).repeat(colspan);
-                    if(colspan > 1){
-                        res += "　　|".repeat(rowshift.shift());
-                    }
-                });
-                res += "　　|".repeat(rowshift.shift());
-                rowshift = [0];
-            }else{
-            $(p).children('td').each(function(j, s){
-                if(j != 0){
-                    let text = ($(s).text() == "")?"　　|":
-                        buildTooLong($(s).text()),
-                        colspan = $(s).attr('colspan') || 1;
-                    if($(s).attr('rowspan') == rowspan){
-                        rowshift[rowshift.length-1] += parseInt(colspan);
-                    } else {
-                        rowshift.push(0);
-                    }
-                    res += text.repeat(colspan);
-                } else{
-                    rowspan = $(s).attr('rowspan');
-                    res += convertFullWidth($(s).text()) + "|";
+            url = "https://pso2.swiki.jp/index.php?%E7%B7%8A%E6%80%A5%E6%8E%B2%E7%A4%BA%E6%9D%BF%2F%E4%BA%88%E5%AE%9A%E8%A1%A8";
+            request({uri: url, strictSSL: false}, function(err, resp, body){
+                if(err){
+                    msg.reply("ERROR: "+err);
+                } else {
+                    parseHtml(msg, body);
                 }
-				if(rowshift[0] == headlen){
-					rowshift[0] = 0;
-				}
             });
-            }
-
-            res = res.slice(0,-4) + "\n"; // forget why 4!
-        });
-        for (var key in TooLong){
-            res += TooLong[key]+": "+key+"\n";
+        } else {
+            parseHtml(msg, body);
         }
-        if( eventList.e == undefined || eventList.e != res){
-            eventList.e = res;
-            msg.reply(res);
-        }else{
-            msg.reply(eventList.e);
-        }
-
       });
 }
 
